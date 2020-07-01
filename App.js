3e766f1daaea0fbc3f66a714e5530b512ec1c70e
Mohamed Aly Sidibe <mouhamed.aly.sidibe96@gmail.com>
2020-07-01 20:36:42 +0000

    The new expo Notifications API skd 38

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 export default function App() {
+  const [visible, setVisible] = React.useState(false);
+  React.useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (response) => {
+        setVisible(true);
+      }
+    );
+    return () => subscription.remove();
+  }, []);
+
+  //ExponentPushToken[iWPGjLKL-vRThXhbT26dVQ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {visible && (
+        <View
+          style={{
+            position: "absolute",
+            top: 40,
+            backgroundColor: "green",
+            width: "90%",
+            height: 100,
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 15 }}>
+            Nouvelle notification
+          </Text>
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
